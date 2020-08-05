68f936dae9f0d69502d17b863c3be50a73f88269
TimWood9 <68300152+TimWood9@users.noreply.github.com>
2020-08-04 21:27:16 -0500

    Added some tests for GameModel class

---
+++ src/main/tests/GameModelTest.java
@@ -0,0 +1,72 @@
+import javafx.animation.Animation;
+import javafx.application.Platform;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import swe.group_nine.controller.GameController;
+import swe.group_nine.controller.Square;
+import swe.group_nine.model.Difficulty;
+import swe.group_nine.model.GameModel;
+
+import static org.junit.Assert.*;
+
+public class GameModelTest {
+    GameModel model;
+    GameController controller;
+
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        Platform.startup(()->{});
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        model = new GameModel(Difficulty.EASY);
+    }
+
+    @Test
+    public void setGrid() {
+    }
+
+    @Test
+    public void getNeighbors() {
+    }
+
+    @Test
+    public void gridIsSet() {
+        assertTrue(model.gridIsSet());
+    }
+
+    @Test
+    public void reset() {
+        model.reset();
+    }
+
+    @Test
+    public void disableAllSquares() {
+        model.disableAllSquares();
+        for(Square[] row : GameModel.grid) {
+            for (Square square : row) {
+                assertTrue(square.isDisabled());
+            }
+        }
+    }
+
+    @Test
+    public void gameOver() {
+        controller = new GameController(Difficulty.EASY);
+        controller.getTimer();
+        GameModel.gameLost = true;
+
+        GameModel.gameOver();
+        assertTrue(GameModel.gameOver);
+
+        assertEquals(Animation.Status.STOPPED, GameController.timer.getTimeline().getStatus());
+
+        assertFalse(GameModel.gameWon);
+
+        GameModel.gameLost = false;
+        GameModel.gameOver();
+        assertTrue(GameModel.gameWon);
+    }
+}
